f5e0dbb8c82b2b4892ad60c5d277d57ef8559f8c
lr94 <lucarobbiano@gmail.com>
2017-10-20 20:07:34 +0200

    Add spaces

--- include/arch/aarch64.h
+++ include/arch/aarch64.h
@@ -5,12 +5,12 @@ enum {D0 = 0, D1, D2, D3, D4, D5, D6, D7,
       D16, D17, D18, D19, D20, D21, D22, D23,
       D24, D25, D26, D27, D28, D29, D30, D31};
 
-#define FADD(x,y,z)     code[i++] = 0x1e602800 | (x) | ((y) << 5) | ((z) << 16)
-#define FSUB(x,y,z)     code[i++] = 0x1e603800 | (x) | ((y) << 5) | ((z) << 16)
-#define FMUL(x,y,z)     code[i++] = 0x1e600800 | (x) | ((y) << 5) | ((z) << 16)
-#define FDIV(x,y,z)     code[i++] = 0x1e601800 | (x) | ((y) << 5) | ((z) << 16)
+#define FADD(x,y,z)         code[i++] = 0x1e602800 | (x) | ((y) << 5) | ((z) << 16)
+#define FSUB(x,y,z)         code[i++] = 0x1e603800 | (x) | ((y) << 5) | ((z) << 16)
+#define FMUL(x,y,z)         code[i++] = 0x1e600800 | (x) | ((y) << 5) | ((z) << 16)
+#define FDIV(x,y,z)         code[i++] = 0x1e601800 | (x) | ((y) << 5) | ((z) << 16)
 
-#define FMOV(x,y)       code[i++] = 0x1e604000 | (x) | ((y) << 5)
+#define FMOV(x,y)           code[i++] = 0x1e604000 | (x) | ((y) << 5)
 
-#define RET             code[i++] = 0xd65f03c0
+#define RET                 code[i++] = 0xd65f03c0
 #endif
